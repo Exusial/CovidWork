88a961eb1cb73f7bf5f07f1fe0de0372ead8363a
defineZYP <50512778+defineZYP@users.noreply.github.com>
2020-09-04 20:15:49 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/covidnews/NewsDataBase/NewsInit.java
@@ -0,0 +1,51 @@
+package com.example.covidnews.NewsDataBase;
+
+import android.os.Handler;
+
+import com.example.covidnews.MainActivity;
+import com.example.covidnews.NetParser.EventsParser;
+import com.example.covidnews.NetParser.NewsParser;
+import com.example.covidnews.listviews.NewsAdapter;
+import com.example.covidnews.listviews.NewsItem;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class NewsInit {
+    private NewsAdapter adapter;
+    private Handler mHandler = null;
+    public NewsInit(NewsAdapter adapter){
+        this.adapter = adapter;
+        mHandler = new Handler();
+    }
+
+    public void InitPage(){
+        new Thread(new ParseAndAdd()).start();
+    }
+
+    private class ParseAndAdd implements Runnable{
+        @Override
+        public void run() {
+            NewsDataBase newsDataBase = NewsDataBase.getDataBase("NewsTest.db");
+            EventsParser eventsParser = EventsParser.getInstance();
+            eventsParser.ParseEvents();
+
+            while(newsDataBase.getCount() < 20);
+
+            ArrayList<News> newsArrayList = newsDataBase.getAll();
+            Random random = new Random();
+            NewsParser newsParser = new NewsParser(MainActivity.getMainActivity());
+            for(int i =0; i <= 9; i++){
+                News news = newsArrayList.get(random.nextInt(newsArrayList.size()));
+                final NewsItem ni = new NewsItem(news.getTitle(), news.getTime(), null);
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.addData(ni);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        }
+    }
+}
