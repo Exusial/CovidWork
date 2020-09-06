60ab2795412fa3cb58e2d1b35bc952999b8c311d
defineZYP <50512778+defineZYP@users.noreply.github.com>
2020-09-06 08:33:34 +0800

    Update LoadMore.java

--- app/src/main/java/com/example/covidnews/Fresher/LoadMore.java
+++ app/src/main/java/com/example/covidnews/Fresher/LoadMore.java
@@ -1,10 +1,12 @@
 package com.example.covidnews.Fresher;
 
 import android.os.Handler;
+import android.util.Log;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.covidnews.MainActivity;
+import com.example.covidnews.NetParser.EventsParser;
 import com.example.covidnews.NewsDataBase.News;
 import com.example.covidnews.NewsDataBase.NewsDataBase;
 import com.example.covidnews.listviews.NewsAdapter;
@@ -20,28 +22,37 @@ public class LoadMore implements Runnable{
     private NewsAdapter adapter;
     private RefreshLayout refreshLayout;
     private NewsFragment fragment;
-    public LoadMore(NewsAdapter adapter, NewsFragment fragment){
+    private String kind;
+    public LoadMore(NewsAdapter adapter, NewsFragment fragment, String kind){
         this.adapter = adapter;
         mHandler = new Handler();
         this.fragment = fragment;
+        this.kind = kind.toLowerCase();
     }
 
     @Override
     public void run() {
-        NewsDataBase newsDataBase1 = NewsDataBase.getDataBase("NewsTest.db");
-        ArrayList<News> newsArrayList = newsDataBase1.getAll();
+        NewsDataBase newsDataBase = NewsDataBase.getDataBase("NewsTest.db");
+        int size = (int)newsDataBase.getCount();
+        Log.d("KIND:", kind);
+        int limit = 100;
         Random random = new Random();
-        for(int i =0; i <= 5; i++) {
-            if (i < newsArrayList.size()) {
-                News news = newsArrayList.get(random.nextInt(newsArrayList.size()));
-                final NewsItem ni = new NewsItem(news.getTitle(), news.getTime(), null);
-                mHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        fragment.freshNews(adapter, ni, -1);
-                    }
-                });
-            }
+        EventsParser eventsParser = new EventsParser();
+        Log.d("GO TO PARSE", "");
+        ArrayList<News> newsArrayList = eventsParser.ParseNewEvents(1, limit, kind);
+        for(int i = 0; i <= 5; i ++){
+            int posi = random.nextInt(100);
+            News news = newsArrayList.get(posi);
+            Log.d("",news.getTitle());
+            Log.d("",news.getId());
+            final NewsItem ni = new NewsItem(news.getTitle(), news.getTime(), null);
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    fragment.freshNews(adapter, ni, -1);
+                }
+            });
         }
+
     }
 }
