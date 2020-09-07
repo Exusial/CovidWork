034fd2397440e060c45052ee08a75332a5935a40
defineZYP <50512778+defineZYP@users.noreply.github.com>
2020-09-07 20:10:59 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/covidnews/NewsDataBase/TimeChecker.java
@@ -0,0 +1,27 @@
+package com.example.covidnews.NewsDataBase;
+
+import android.annotation.SuppressLint;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class TimeChecker {
+    public static String CheckString(String Time){
+        @SuppressLint("SimpleDateFormat")
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String tm = Time.split(" ")[0];
+        try {
+            formatter.parse(tm);
+            return Time;
+        } catch (ParseException e) {
+            String[] s = Time.split("/");
+            StringBuilder f = new StringBuilder(s[0]);
+            for(int i = 1; i <= 2; i++){
+                f.append("-");
+                f.append(s[i]);
+            }
+            return f.toString();
+        }
+    }
+}
